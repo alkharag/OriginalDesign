b6efaad8bd1c2b18b84bd203820edd189e5261e8
alkharag <kharag.alex_sa@yahoo.com>
2014-08-29 09:59:22 -0700

    made a car

--- OriginalDesign.pde
+++ OriginalDesign.pde
@@ -1,9 +1,40 @@
+int xPosition = 150;
+	
+	
 void setup()
 {
-
+	size(512, 512);
 }
 void draw()
 {
-  ellipse(50,50,50,50);
-}
+	
+	ball();
 
+}
+void ball()
+{
+	background(50,50,90);
+
+	rect(0, 300, 500, 512);
+	
+	//body
+	arc(xPosition-25, 300, 200, 75, PI, 2*PI);
+
+	//Wheels
+	fill(0);
+	strokeWeight(3);
+	stroke(255);
+	ellipse(xPosition, 300,30,30);
+	ellipse(xPosition -100 , 300,30,30);
+
+	xPosition+=10;
+	
+
+	if (xPosition > 630){
+		xPosition = -170;
+	}
+	
+	redraw();
+	
+
+}
--- build-tmp/source/OriginalDesign.java
+++ build-tmp/source/OriginalDesign.java
@@ -14,15 +14,46 @@ import java.io.IOException;
 
 public class OriginalDesign extends PApplet {
 
+int xPosition = 150;
+	
+	
 public void setup()
 {
-
+	size(512, 512);
 }
 public void draw()
 {
-  ellipse(50,50,50,50);
-}
+	
+	ball();
 
+}
+public void ball()
+{
+	background(50,50,90);
+
+	rect(0, 300, 500, 512);
+	
+	//body
+	arc(xPosition-25, 300, 200, 75, PI, 2*PI);
+
+	//Wheels
+	fill(0);
+	strokeWeight(3);
+	stroke(255);
+	ellipse(xPosition, 300,30,30);
+	ellipse(xPosition -100 , 300,30,30);
+
+	xPosition+=10;
+	
+
+	if (xPosition > 630){
+		xPosition = -170;
+	}
+	
+	redraw();
+	
+
+}
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "OriginalDesign" };
     if (passedArgs != null) {
